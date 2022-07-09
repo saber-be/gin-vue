a98d7f6753e935273c2afd544b416b5bf9de4bdb
saber bejestani <saberbejestani@gmail.com>
2022-07-09 14:28:59 +0300

    searh users by name (#6)

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
-createApp(App).use(ElementPlus).mount('#app')
+import { vue3Debounce } from 'vue-debounce'
+createApp(App).use(ElementPlus).directive('debounce', vue3Debounce({ lock: true })).mount('#app')
