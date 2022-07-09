d7c3a2960063919463894b4bfedcdced8890ac26
saber <saberbejestani@gmail.com>
2022-07-09 16:51:08 +0300

    delete User by ID

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -13,12 +13,24 @@
         <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
           >Edit</el-button
         >
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)"
-          >Delete</el-button
+        <el-popconfirm
+          confirm-button-text="Yes"
+          cancel-button-text="No, Thanks"
+          :icon="InfoFilled"
+          icon-color="#626AEF"
+          title="Are you sure to delete this?"
+          @confirm="confirmDeleteEvent"
+          @cancel="cancelEvent"
         >
+          <template #reference>
+            <el-button
+                size="small"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)"
+                >Delete</el-button>
+          </template>
+        </el-popconfirm>
+        
       </template>
     </el-table-column>
   </el-table>
@@ -35,7 +47,7 @@
 
 <script>
 
-
+import { ElNotification } from 'element-plus'
 export default {
   name: 'App',
   components: {
@@ -45,6 +57,7 @@ export default {
     return {
       search : '',
       users : [],
+      selected_user : {},
       pagination : {
         page : 1,
         limit : 12,
@@ -64,8 +77,26 @@ export default {
           this.pagination = json.pagination;
         })
     },
+    deleteUsers(id) {
+      fetch('http://localhost:8086/api/v1/users/'+id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          this.getUsers();
+          ElNotification({
+            title: 'Success',
+            message: 'User deleted successfully',
+            type: 'success',
+          })
+        })
+    },
     prevPage(val) {
-      this.pagination.page = val
+      this.pagination.page = val;
     },
     nextPage(val) {
       this.pagination.page = val;
@@ -74,6 +105,19 @@ export default {
       this.pagination.page = val;
       this.getUsers();
     },
+    handleDelete(index, row){
+      console.log(index, row.id);
+      this.selected_user = row;
+    },
+    confirmDeleteEvent() {
+      console.log(this.selected_user)
+      this.deleteUsers(this.selected_user.id);
+    },    
+    cancelEvent() {
+      console.log('cancel');
+      this.selected_user = {};
+    },
+
   }
 }
 </script>
