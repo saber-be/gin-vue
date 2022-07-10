d2be3ec1803074b72860e9c08bd51f2e526c770b
saber <saberbejestani@gmail.com>
2022-07-10 14:49:12 +0300

    make user's email unique

--- service/models/user.go
+++ service/models/user.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	ID    uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name  string `json:"name"`
-	Email string `json:"email"`
+	Email string `json:"email" gorm:"type:varchar(100);unique"`
 	Phone string `json:"phone"`
 	Age   uint   `json:"age"`
 }
@@ -12,5 +12,5 @@ type UserInput struct {
 	Name  string `json:"name" binding:"required"`
 	Email string `json:"email" binding:"required,email"`
 	Phone string `json:"phone" binding:"required"`
-	Age   uint   `json:"age,string" binding:"required,min=18"`
+	Age   uint   `json:"age" binding:"required,min=18"`
 }
